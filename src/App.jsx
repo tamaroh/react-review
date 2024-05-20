ddbb2b839c53397df0e02581b57df13b5140f625
Tamaroh <tamaroh@gmail.com>
2024-05-20 11:05:36 +0900

    created

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,28 @@ function PokemonElement () {
   if (pokemon === "") {
     return <p>pokemon name displayed here</p>;
   } else {
+    console.log(pokemon);
     return (
       <>
-        <dt>name</dt>
-        <dd>{pokemon.name}</dd>
-        <dt>height</dt>
-        <dd>{pokemon.height} m</dd>
-        <dt>weight</dt>
-        <dd>{pokemon.weight} kg</dd>
+        <h2>{pokemon.name}</h2>
+        <img src={pokemon.sprites.front_default} alt="pokemon sprite" />
       </>
     )
   }
 }
 
-function fetchData() {
+async function fetchData(idOrName) {
+  const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`)
+  await result.json()
+  .then(res => setPokemon(res));
+  
 }
 
   return (
     <>
     <h1>pokemon api</h1>
     <input ref={inputText}></input>
-    <button onClick={() => fetchData(inputText.current.value)}>send</button>
+    <button onClick={() => (fetchData(inputText.current.value))}>ポケモンゲットだぜ！</button>
     <div id="result">
       <PokemonElement></PokemonElement>
     </div>
